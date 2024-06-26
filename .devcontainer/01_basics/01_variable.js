bd64cdd64a1b63505c5b2672e6cee30acd2db245
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-06-26 11:16:33 +0000

    day 2 in js(variables)

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,9 @@
+// TOPIC= let, const, var story;
+
+const accountId = "roy@2123456"
+let accountEmail = "roy2005@gmail.com"
+var accountPassword = 123456
+let accountAdress = "benagaluru"
+let cgpa ;
+console.table([accountEmail,accountPassword,accountId,accountAdress,cgpa])
+console.table(type of[accountAdress, accountEmail.accountId,accountPassword,cgpa]);
