05b9b315fcf91e7f66c2f012e122733dd9bd147d
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-07-03 11:15:45 +0000

    datatypes_summary.js

---
+++ .devcontainer/01_basics/datatypes_Summary.js
@@ -0,0 +1,25 @@
+// DATA TYPES are 2 types =
+//1) Premitive DataType = nuber, string, boolean, BigInt, symbol, undefined;
+
+// 2)Reference(Non Premitive Datatypes) = Array, OBject,Functions;
+
+const score = 100
+const scoreValue = 100.4
+
+const outsideTemp = null
+let isLoggedIn = false
+const id = Symbol('1234')
+let eamil ;
+const anotherId = Symbol('1234')
+console.log(id === anotherId);
+
+
+// Non prmitive=
+
+ const heroes = ["IronMan", "Hulk", "Deadpool", "Shaktiman"];
+ let Obj={
+  nanme : "Mayank Roy",
+  RollNo : 119001230760,
+  semster :"1st",
+ }
+
