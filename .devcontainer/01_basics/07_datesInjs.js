bfa195d6077caff4fca7497c350a03760f7e393e
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-07-11 18:34:08 +0000

    07_Dates_In_js

--- .devcontainer/01_basics/07_datesInjs.js
+++ .devcontainer/01_basics/07_datesInjs.js
@@ -9,8 +9,13 @@ console.log(typeof myDate)
 let myCreatedDate = new Date(2023, 0, 23) //all javascript started month from zero
 console.log(myCreatedDate);
 console.log(myCreatedDate.toDateString());
-
-let myTimeStamp = Date.now()
+                                
+let myTimeStamp = Date.now();
 console.log(myTimeStamp);
-
-console.log(myCreatedDate.getTime);
+                      
+let myCreatedDateOne = new Date();
+console.log(myCreatedDateOne.getTime());
+console.log(myTimeStamp.toString());
+console.log(Date.now()); ///it will show milliseconds
+// Assuming myCreatedDate is not a function, remove the incorrect call
+// console.log(myCreatedDate(Math.floor(Date.now()/1000)));
