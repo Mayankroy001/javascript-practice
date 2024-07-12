a4d2d43073d5f0bb4ab78010a28c30190b87907d
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-07-12 19:50:50 +0000

    02_Arrays.js

--- .devcontainer/02_basics/02_Arrays.js
+++ .devcontainer/02_basics/02_Arrays.js
@@ -7,8 +7,8 @@ console.log(typeof marvelHeroes);
 
 marvelHeroes.push(dcHeroes) //here push actually not add the two arrays it's actually merge the array of arrays
 //console.log(marvelHeroes);
- marvelHeroes.flat(i)
-  
+ const allHEROES = [marvelHeroes, dcHeroes];
+  const allHEROES1 = allHEROES.flat(Infinity);
 
 ////////To solve this problem we are going to use(.concat)
 
@@ -19,3 +19,25 @@ console.log(marvelHeroes_1);
 
 const allHeroes = [...dcHeroes,...marvelHeroes]
 console.log(allHeroes);
+
+
+///
+
+console.log(Array.isArray("Mayank"));// but you can convert this string into array
+console.log(Array.from("Mayank"));    //(.from) is a funtion that convert any object to array.
+
+
+let object ={
+ score1 : 100,
+ score2 : 200,
+ score3 : 300,
+}
+
+let object2 ={
+    Nmae : "M",
+    cgpa : 8,
+    roll : 3
+}
+const allObject = [object,object2];
+console.log(Array.from(allObject));
+console.log(Array.of(object));
