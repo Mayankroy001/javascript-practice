dd68f9219a9824662c02d472ed6e32828caf859e
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-07-20 19:20:40 +0000

    01_function.js

---
+++ .devcontainer/03_basics/01_Functions.js
@@ -0,0 +1,29 @@
+function sayMyName(){
+    console.log("M");
+    console.log("A");
+    console.log("y");
+    console.log("A");
+    console.log("N");
+    console.log("k");
+}
+
+ sayMyName();
+   
+ /////////////////sum
+ let exaple ={
+ function addTwoNumbers(number1, numbe2){
+
+    let  result : number1 + numbe2
+     return number1 + numbe2
+ }
+ const result : addTwoNumbers(3,5);
+ console.log(result);
+}
+ //////////////// minus
+
+ function subTwoNumbers(number3 , number4){
+    let result =number3 - number4
+    const result2 = subTwoNumbers(5,4);
+    console.log(result2);
+       
+}
